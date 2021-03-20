2ba672151d86581c24e0db9459ef9f0fffaa172f
Mark Clegg <mark.clegg2@gmail.com>
2021-03-20 20:30:09 +0000

    Preparation of code and data segments

--- BIOS/BIOS.asm
+++ BIOS/BIOS.asm
@@ -1,26 +1,31 @@
-; Elf-Cube BIOS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;       Elf-Cube BIOS
+;
+;       Version:    0.1
+;       Author:     Mark Clegg
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
         CPU     1802
 
-	INCLUDE	"macros.inc"
+        INCLUDE "macros.inc"
 
-ORIGIN	EQU	$8000
-STACK	EQU	$FEFF
+ORIGIN  EQU     $8000       ; Main entry point
+DATA    EQU     $FF00       ; BIOS Data Area (Top of RAM)
+STACK   EQU     DATA-1      ; Stack is directly under the BIOS Data Area
 
+        SEG     DATA        ; Setup the BIOS Data Area segment
+        ORG     DATA
+        
+        SEG                 ; Setup the default code segment
         ORG     ORIGIN
+        
         DIS
         DB      $00
-;
-;       Setup Stack
-;
-        LOAD    R2, STACK
-;
-;       Switch to main program counter on R3
-;
-        LOAD    R3, START
+
+        LOAD    R2, STACK   ; Initialise the Stack Pointer (R2)
+        LOAD    R3, START   ; and switch to R3 as the Program Counter
         SEP     R3
-;
-;       Built-in functions
-;
+        
 START   
 
         END     ORIGIN
